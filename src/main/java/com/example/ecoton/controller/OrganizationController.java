d00cb4ca68b977cd1413fe234667bf1ed718ce86
nursultanKanybekov <nursultanKanybekov12082000@gmail.com>
2023-12-06 21:22:43 +0600

    first commit

--- src/main/java/com/example/ecoton/controller/OrganizationController.java
+++ src/main/java/com/example/ecoton/controller/OrganizationController.java
@@ -1,4 +1,13 @@
-package com.example.ecoton;
+package com.example.ecoton.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class OrganizationController {
+    @RequestMapping(value = "/organization", method = RequestMethod.GET)
+    public String aboutG(){
+        return "organization";
+    }
 }
